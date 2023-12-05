2e1e11baf41593237ee69923af3746e34e7860be
Буянова Татьяна Сергеевна <TSergBuyanova@sberbank.ru>
2023-12-05 09:17:28 +0300

    init commit

---
+++ src/test/java/ru/tms/TmsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
